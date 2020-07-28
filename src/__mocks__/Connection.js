2bd358838e2cd34ffb702985ff5df385f41e81be
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2020-07-28 23:16:32 +0500

    Handle web socket connection status

--- src/__mocks__/Connection.js
+++ src/__mocks__/Connection.js
@@ -7,4 +7,15 @@ export default class Connection extends EventTarget {
   init() {
     this.dispatchEvent(new Event('wsOpen'));
   }
+
+  close() {
+    this.dispatchEvent(new Event('wsClose'));
+  }
+
+  error() {
+    const error = new Error('some error');
+    const event = new Event('wsError');
+    event.error = error;
+    this.dispatchEvent(event);
+  }
 }
